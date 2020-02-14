39ddaea599ee32f3bf6523d02e957642deb57bb0
mtamahori <mtamahori@gmail.com>
2020-02-14 14:48:56 -0600

    refactor routes

--- server/api/spotify.js
+++ server/api/spotify.js
@@ -5,53 +5,13 @@ const { tokenRefresh } = require('../middleware/spotifyTokenRefresh');
 
 module.exports = router;
 
-router.get('/recentlyPlayedTracks', tokenRefresh, (req, res, next) => {
+router.use('/', tokenRefresh, (req, res, next) => {
   spotifyApi.setAccessToken(req.user.access)
-  return spotifyApi.getMyRecentlyPlayedTracks({
-    limit: 4
-  })
-  .then(data => {
-    res.send(data.body.items)
-  })
-  .catch(next)
+  next();
 })
 
-router.get('/topTracks', tokenRefresh, (req, res, next) => {
-  spotifyApi.setAccessToken(req.user.access)
-  return spotifyApi.getMyTopTracks({
-    limit: 4
-  })
-  .then(data => {
-    res.send(data.body.items)
-  })
-  .catch(next)
-})
-
-router.get('/savedTracks', tokenRefresh, (req, res, next) => {
-  spotifyApi.setAccessToken(req.user.access)
-  return spotifyApi.getMySavedTracks({
-    limit: 4
-  })
-  .then(data => {
-    res.send(data.body.items)
-  })
-  .catch(next)
-})
-
-router.get('/savedAlbums', tokenRefresh, (req, res, next) => {
-  spotifyApi.setAccessToken(req.user.access)
-  return spotifyApi.getMySavedAlbums({
-    limit: 4
-  })
-  .then(data => {
-    res.send(data.body.items)
-  })
-  .catch(next)
-})
-
-router.get('/topArtists', tokenRefresh, (req, res, next) => {
-  spotifyApi.setAccessToken(req.user.access)
-  return spotifyApi.getMyTopArtists({
+router.get('/:endpoint', (req, res, next) => {
+  return spotifyApi['getMy'+req.params.endpoint]({
     limit: 4
   })
   .then(data => {
